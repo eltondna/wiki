23249c1eeba8438aaf0787dc813356cb19ab703f
Wong Ming Yan <wongmingyan@Wongs-MacBook-Air.local>
2022-12-17 13:00:20 +0800

    add page content

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
-
 from . import util
+from django.http import HttpResponseNotFound
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def pages(request, page):
+    # chenck whether page route exist 
+    page_content  = util.get_entry(page)
+
+    # Return page content if it exists
+    if ( page_content != None):
+        return render(request, "encyclopedia/pages.html",{
+            "page" : page.capitalize(),
+            "content":page_content
+        })
+
+    #Return Error when page not exist 
+    else:
+        return HttpResponseNotFound(f"Cannot find the ' {page} ' page ")
+
+
+
+
+        
+
+
+
 
